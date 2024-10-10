39600a47ab5aec8cf49e09a303021886a308916e
Kethillyn <naykethillyn@gmail.com>
2024-10-10 10:06:00 -0300

    O[cond

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Sweet-Spot0",
-})
+  base: "/Sweet-Spot",
+});
